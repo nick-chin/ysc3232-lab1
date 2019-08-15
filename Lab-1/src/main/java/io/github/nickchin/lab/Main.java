477173bd1bf1b4903f787cb581b3facee17ae0d8
Nick Chin <nickchin00@gmail.com>
2019-08-15 11:13:24 +0800

    added comments/documentation

--- Lab-1/src/main/java/io/github/nickchin/lab/Main.java
+++ Lab-1/src/main/java/io/github/nickchin/lab/Main.java
@@ -17,23 +17,29 @@ public class Main {
      * @param args any String array, but won't be used
      */
     public static void main(String[] args) {
+        // 0 arguments test (extra)
         System.out.println("Here is an example of 0 arguments minMaxAvg.");
         minMaxAvg(new double[0]);
         
+        // 3 arguments test (required)
         System.out.println("Here is an example of 3 arguments in minMaxAvg.");
         double[] nums = new double[3];
-        int floor = -10;
-        int ceiling = 10;
-        int range = ceiling - floor + 1;
+        int floor = -10; // smallest value in random number generation
+        int ceiling = 10; // largest value in random number generation 
+        int range = ceiling - floor + 1; // range of numbers
         for (int i = 0; i < nums.length; i++) {
+            // generating 3 numbers in range and assigning it to nums array
             nums[i] = (Math.random() * range) + floor;
         }
         System.out.println("3 arguments are: " + Arrays.toString(nums));
         minMaxAvg(nums);
         
+        // any number of arguments (extra)
         System.out.println("Here is an example of random number of arguments.");
         double[] longNums = new double[(int)(Math.random() * 20) + 1];
         for (int j = 0; j < longNums.length; j++) {
+            // generating longNums.length amount of random numbers and
+            // assigning them to longNums
             longNums[j] = (Math.random() * range) + floor;
         }
         System.out.println(longNums.length + " arguments are: " + 
